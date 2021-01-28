258dc26473792257a76bf86a8e324001f9ce9a0c
Peace <onyeepeaz@gmail.com>
2021-01-28 19:37:51 +0100

    changed image size for og tags

--- components/layout.js
+++ components/layout.js
@@ -27,13 +27,15 @@ export default function Layout({ children }) {
                 <meta property="og:title" content="RetroGram" />
                 <meta property="og:site_name" content="RetroGram" />
                 <meta property="og:url" content="https://retrogram.co" />
-                <meta property="og:image" content="https://pbs.twimg.com/profile_banners/1353647172391231492/1611744920/1500x500" />
+                <meta property="og:image" content="https://i.postimg.cc/cCnDjhp6/2020BG.png" />
+                <meta property="og:image:width" content="1200" />
+                <meta property="og:image:height" content="627" />
                 <meta name="og:description" content="Want to learn from others? Read their year in review to learn from their wins and loses" />
                 <meta name="twitter:card" content="summary_large_image" />
                 <meta name="twitter:site" content="@RetroGramHQ" />
                 <meta name="twitter:title" content="RetroGram" />
                 <meta name="twitter:description" content="Want to learn from others? Read their year in review to learn from their wins and loses" />
-                <meta name="twitter:image" content="https://pbs.twimg.com/profile_banners/1353647172391231492/1611744920/1500x500" />
+                <meta name="twitter:image" content="https://i.postimg.cc/cCnDjhp6/2020BG.png" />
                 <meta name="twitter:image:alt" content="RetroGram logo"></meta>
             </Head>
             <header className={navStyles.header}>
